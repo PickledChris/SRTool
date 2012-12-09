7646671980d16caa2bb38b848ce36de9085eb3ed
Chris Keeley <pickledjesus@gmail.com>
2012-12-09 19:04:37 +0000

    Fixed LNOT?

--- src/srt/tool/ExprToSmtlibVisitor.java
+++ src/srt/tool/ExprToSmtlibVisitor.java
@@ -118,7 +118,7 @@ public class ExprToSmtlibVisitor extends DefaultVisitor {
 			operator = "(%s)";
 			break;
 		case UnaryExpr.LNOT: // possibly broken
-			operator = "(tobv32 (not (bvnot %s)))";
+			operator = "(tobv32 (not (tobool %s)))";
 			break;
 		case UnaryExpr.BNOT:
 			operator = "(bvnot %s)";
