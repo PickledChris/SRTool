0489a1718d99936ae9b1e4a473fa65ceeb38570f
Chris Bates <blokeyking@gmail.com>
2012-12-11 18:21:55 +0000

    LoopUnwiderVisitor. Not fully tested.

--- src/srt/tool/LoopUnwinderVisitor.java
+++ src/srt/tool/LoopUnwinderVisitor.java
@@ -1,7 +1,10 @@
 package srt.tool;
 
-import srt.ast.WhileStmt;
+import srt.ast.*;
 import srt.ast.visitor.impl.DefaultVisitor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoopUnwinderVisitor extends DefaultVisitor {
 
@@ -17,7 +20,41 @@ public class LoopUnwinderVisitor extends DefaultVisitor {
 
 	@Override
 	public Object visit(WhileStmt whileStmt) {
-		return super.visit(whileStmt);
+
+        IntLiteral bound = whileStmt.getBound();
+
+        // unroll once.
+        Expr condition = whileStmt.getCondition();
+        Stmt body = whileStmt.getBody();
+
+        Stmt ifStmt = whileVerificationStmt(condition);
+        // start for at 1 because one unroll already done
+        for(int i = 0; i < bound.getValue(); i++) {
+
+            // use body + the ifStatement generated previously to make up the next body
+            List<Stmt> unrolledIfBody = new ArrayList<Stmt>();
+            unrolledIfBody.add(body);
+            unrolledIfBody.add(ifStmt);
+            StmtList newIfBody = new StmtList(unrolledIfBody);
+
+            ifStmt = new IfStmt(condition, new BlockStmt(newIfBody), new EmptyStmt());
+
+
+        }
+
+		return super.visit(ifStmt);
 	}
 
+    private Stmt whileVerificationStmt(Expr condition) {
+        List<Stmt> assumeAssert = new ArrayList<Stmt>();
+        assumeAssert.add(new AssertStmt(negate(condition)));
+        assumeAssert.add(new AssumeStmt(negate(condition)));
+        StmtList list = new StmtList(assumeAssert);
+        return new BlockStmt(list);
+    }
+
+    private Expr negate(Expr expr) {
+        Expr expression = new UnaryExpr(UnaryExpr.LNOT, expr);
+        return expr;  //To change body of created methods use File | Settings | File Templates.
+    }
 }
