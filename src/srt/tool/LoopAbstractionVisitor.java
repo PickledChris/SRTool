b288d527e946beb5cbf06c5fb2bded0453c3bf0a
Michael Thorpe <mt2309@ic.ac.uk>
2012-12-13 14:27:36 +0000

    Work on LoopAbstraction.

--- src/srt/tool/LoopAbstractionVisitor.java
+++ src/srt/tool/LoopAbstractionVisitor.java
@@ -1,7 +1,10 @@
 package srt.tool;
 
-import srt.ast.WhileStmt;
+import srt.ast.*;
 import srt.ast.visitor.impl.DefaultVisitor;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class LoopAbstractionVisitor extends DefaultVisitor {
 
@@ -11,7 +14,26 @@ public class LoopAbstractionVisitor extends DefaultVisitor {
 
 	@Override
 	public Object visit(WhileStmt whileStmt) {
-		return super.visit(whileStmt);
+
+        List<Expr> exprs = whileStmt.getInvariantList().getExprs();
+        int length = exprs.size();
+
+        Expr cond;
+
+        if (length > 0) {
+            // expr && expr = expr, just vaguely more inefficient
+            cond = exprs.get(0);
+
+            for(Expr e : exprs)
+                cond = new BinaryExpr(BinaryExpr.LAND,cond,e);
+        }
+        else {
+            return super.visit(whileStmt);
+        }
+
+        Expr condition = new BinaryExpr(BinaryExpr.LAND,whileStmt.getCondition(),cond);
+
+		return super.visit(new WhileStmt(condition,whileStmt.getBound(),whileStmt.getInvariantList(),whileStmt.getBody()));
 	}
 
 }
